eadb448c274f721d145d17239efe02e249dcc50e
idriss-duval <idrisscoder@gmail.com>
2021-11-15 22:34:48 +0100

    Reformatage du code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let childrens = ["Idriss", "Carel", "Brice", "Franck"];
 
-let pseudo = childrens.map((children)=>children+""+Math.random(3,4567));
+let pseudo = childrens && childrens.map((children)=>children+""+Math.random(3,4567));
 
-pseudo.forEach((name)=>console.log("Hello " + name));
+pseudo && pseudo.forEach((name)=>console.log("Hello " + name));
 
