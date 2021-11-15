1574b80f088c9ff22f7a35c636fdfc06471c3be5
idriss-duval <idrissduval2@gmail.com>
2021-11-15 21:50:34 +0100

    Correction de la mise en forme du code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let childrens = ["Idriss", "Carel", "Brice", "Franck"];
+
 let pseudo = childrens.map((children)=>children+""+Math.random(3,4567));
 
-pseudo.forEach((name)=>console.log("Hello "+name))
+pseudo.forEach((name)=>console.log("Hello " + name));
 
